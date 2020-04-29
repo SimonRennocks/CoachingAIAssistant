c1a26d821ec2338dc05bb1b8033ccb9380c057d0
Simon Rennocks <rennocks@gmail.com>
2020-04-29 00:21:06 -0400

    server update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(routes);
 
 app.listen(PORT, () => {
