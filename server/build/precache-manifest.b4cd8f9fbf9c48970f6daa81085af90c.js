38c8bf3a703c5ff54e716af196419428448a7ac6
Simon Rennocks <rennocks@gmail.com>
2020-04-29 00:16:54 -0400

    server update

---
+++ server/build/precache-manifest.b4cd8f9fbf9c48970f6daa81085af90c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720260e446f48f9a8aacb4b55900568d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1669a98161b664b3e35c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9cab2a11086a8ac2a8d7",
+    "url": "/static/js/2.0d5ae498.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d5ae498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1669a98161b664b3e35c",
+    "url": "/static/js/main.b95d9e8c.chunk.js"
+  },
+  {
+    "revision": "48f1a1f2249b65a0d97c",
+    "url": "/static/js/runtime-main.e7f299ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
