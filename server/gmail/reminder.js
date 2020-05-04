16368d5d77f40119045df3261003c36e0ef84792
Simon Rennocks <rennocks@gmail.com>
2020-05-04 15:19:48 -0400

    gmail reminder changes

--- server/gmail/reminder.js
+++ server/gmail/reminder.js
@@ -17,12 +17,6 @@ fs.readFile('credentials.json', (err, content) => {
     authorize(JSON.parse(content), main);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
 function authorize(credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
@@ -36,12 +30,6 @@ function authorize(credentials, callback) {
     });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
 function getNewToken(oAuth2Client, callback) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
@@ -67,95 +55,79 @@ function getNewToken(oAuth2Client, callback) {
     });
 }
 
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
 
 
+function main(auth) {
+    data = JSON.parse(fs.readFileSync("reminders.json"))
+    data.reminders.forEach(day => {
+        day.sessions.forEach(session => {
+            let student = data.students.filter(student => student.email === session.to)[0];
+            let studentsession = {...day,  ...session, ...student};
+            var raw = makeBody(studentsession, false);
+            console.log(day.send + " " + day.date + " " + studentsession.name + " " + studentsession.time)
+            if (day.send) {
+                const gmail = google.gmail({ version: 'v1', auth });
+                gmail.users.messages.send({
+                    userId: 'me',
+                    resource: {
+                        raw: raw
+                    }
+                }, (err, res) => {
+                    if (err) return console.log('The API returned an error: ' + err);
+                    console.log(res.statusText);
+                });
+            }
+        });
+    });
+}
 
-function makeBody(from, date, azZone, student, test) {
-    let subject = "";
-    let message = "";
-    let cc = "centraltutorsupport@bootcampspot.com"
-    if (test)
-        cc = ""
+function makeBody(session) {
+    let cc = ""
+    if (!session.ccskip)
+        cc = "centraltutorsupport@bootcampspot.com"
     let bcc = "";
     let template = fs.readFileSync("reminder.html").toString()
     let zone = "";
 
-    let tZone = student.zone;
-    switch (student.zone) {
+    let tZone = session.zone;
+    switch (session.zone) {
         case 0: zone = "Eastern"; break;
         case -1: zone = "Central"; break;
         case -2: zone = "Mountain"; break;
         case -3: zone = "Pacific"; break;
         case -2.5: 
             zone = "Arizona";
-            tZone = azZone;
+            tZone = session.azZone;
             break;
         default: zone = ""
     }
  
-    let time = new Date(date + ", " + student.time);
-    if (student.zone !== 0){
+    let time = new Date(session.date + ", " + session.time);
+    if (session.zone !== 0){
         time.setTime(time.getTime() + (tZone*60*60*1000))
     }
-  
-    let localtime = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const localtime = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
    
-    subject = `Coding Boot Camp - Tutorial Confirmation - ${date}, ${localtime} ${zone}`
+    const subject = `Coding Boot Camp - Tutorial Confirmation - ${session.date}, ${localtime} ${zone}`
+   
     template = Handlebars.compile(template);
-
-    message = template({
-        date: date,
-        name: student.name,
-        zoom: student.zoom,
+    const message = template({
+        date: session.date,
+        name: session.name,
+        zoom: session.zoom,
         time: localtime,
         zone: zone,
     })
 
-    cc = cc.toString().replace(new RegExp('\r?\n', 'g'), ",");
     var str = ["MIME-Version: 1.0\n",
         "Content-Type: text/html; charset=utf-8\n",
-        "reply to: ", from, "\n",
-        "to: ", student.to, "\n",
+        "to: ", session.to, "\n",
         "cc: ", cc, "\n",
         "bcc: ", bcc, "\n",
-        "from: ", from, "\n",
         "subject: ", subject, "\n\n",
         message
     ].join('');
 
     var encodedMail = new Buffer.from(str).toString("base64").replace(/\+/g, '-').replace(/\//g, '_');
     return encodedMail;
-}
-
-function main(auth) {
-
-    data = JSON.parse(fs.readFileSync("reminders.json"))
-    data.reminders.forEach(day => {
-        day.sessions.forEach(session => {
-            let student = data.students.filter(student => student.email === session.to)[0];
-            let studentsession = {...session, ...student};
-            var raw = makeBody('rennocks@gmail.com', day.date, day.azZone, studentsession, false);
-            console.log(day.send + " " + day.date + " " + student.name + " " + student.time)
-            if (day.send) {
-                // const gmail = google.gmail({ version: 'v1', auth });
-                // gmail.users.messages.send({
-                //     userId: 'me',
-                //     resource: {
-                //         raw: raw
-                //     }
-                // }, (err, res) => {
-                //     if (err) return console.log('The API returned an error: ' + err);
-                //     console.log(res.statusText);
-                // });
-            }
-
-        });
-    });
-
-
 }
